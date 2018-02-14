2b58322d93337d1bdd54836fc851be37c0de930b
Ganeshmurthy <ganeshmurthy.p@gmail.com>
2018-02-14 18:55:27 +0530

    summary

--- src/app/flight-book-summary/flight-book-summary.component.ts
+++ src/app/flight-book-summary/flight-book-summary.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Route, RouterModule,Router,ActivatedRoute} from '@angular/router'; 
 import { FlightServiceService } from '../flight-service.service';
 import { NgProgress } from '@ngx-progressbar/core';
+import { Response } from '@angular/http/src/static_response';
 
 @Component({
   selector: 'app-flight-book-summary',
@@ -12,7 +13,13 @@ import { NgProgress } from '@ngx-progressbar/core';
 export class FlightBookSummaryComponent implements OnInit {
 apiData;
 appkey;
+islowfare;
+lowfareflightindex1;
+lowfareflightindex2;
+reasontoselect;
+searchkey;
 flightindex;
+
   constructor(private flightServc:FlightServiceService,
     private router: Router ,
     private actRoute: ActivatedRoute,
@@ -21,15 +28,24 @@ flightindex;
   ngOnInit() {
     this.actRoute.queryParams
     .subscribe(params=>{
-      this.appkey = params['appKey'];
-
-
+      this.appkey = params['app_key'];
+      this.flightindex = params['flightindex'];
+      this.islowfare = params['islowfare'];
+      this.lowfareflightindex1 = params['lowfareflightindex1'];
+      this.lowfareflightindex2 = params['lowfareflightindex2'];
+      this.reasontoselect = params['reasontoselect'];
+      this.searchkey = params['searchkey'];
+      this.apiData = 'app_key='+this.appkey+'&flightindex='+this.flightindex+'&islowfare='+this.islowfare+'&lowfareflightindex1='+this.lowfareflightindex1+'&lowfareflightindex2='+this.lowfareflightindex2+'&reasontoselect='+this.reasontoselect+'&searchkey='+this.searchkey;
       this.getAirpriceSummary();
     })
   }
 
   getAirpriceSummary(){
-    this.apiData = "";
+    this.progress.start();
+    this.flightServc.flightAirPrice(this.apiData).subscribe(response=>{
+      this.progress.done();
+      console.log("Response",response);
+    })
   }
   
 }
